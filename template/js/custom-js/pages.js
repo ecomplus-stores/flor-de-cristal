eb8385acf7bb98af9fb7de8d01c9d4c05938227d
Matheus <matheus@e-com.club>
2020-07-24 21:58:07 -0300

    chore: init layout 1.0

--- template/js/custom-js/pages.js
+++ template/js/custom-js/pages.js
@@ -1,1 +1,3 @@
 // Add your custom JavaScript for storefront pages here.
+const nameIf = window.ecomPassport.customer ? window.ecomPassport.customer.display_name : ''
+document.getElementById('nameIf').innerHTML = 'Olá ' + nameIf
